2b93886574cc93a75609a18c96631063e3e474bb
harlan <isharlan.hu@gmail.com>
2023-12-15 18:07:59 +0800

    Add Dht11 device test
    A new Junit test file 'Dht11Test.java' has been added for testing Dht11 device. It specifically tests the detection() method ensuring that humidity and temperature values for the Dht11 device are not zero.

---
+++ src/test/java/cn/tpkf/pi/devices/gpio/w1/Dht11Test.java
@@ -0,0 +1,31 @@
+package cn.tpkf.pi.devices.gpio.w1;
+
+import cn.tpkf.pi.enums.BCMEnums;
+import cn.tpkf.pi.manager.DeviceManager;
+import com.pi4j.Pi4J;
+import com.pi4j.context.Context;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Harlan
+ * @email isharlan.hu@gmali.com
+ * @date 2023/12/15
+ */
+@Slf4j
+class Dht11Test {
+
+    private final Context context = Pi4J.newAutoContext();
+
+    @Test
+    void detection() {
+        DeviceManager deviceManager = new DeviceManager(context);
+        Dht11 dht11 = new Dht11(deviceManager, "dht11", "温湿度传感器", BCMEnums.BCM_4);
+        dht11.detection();
+        assertNotEquals(0.0, dht11.getHumidity());
+        assertNotEquals(0.0, dht11.getTemperature());
+        log.info("温度：{}，湿度：{}", dht11.getTemperature(), dht11.getHumidity());
+    }
+}
