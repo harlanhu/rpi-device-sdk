e643f908c9561814db6787bf1a0a486f4d1063ec
harlan <isharlan.hu@gmail.com>
2023-12-15 18:02:35 +0800

    Refactor Dht11 class and improve detection method
    The name of the Dht11 class was corrected from Dth11 to Dht11. The detection method was significantly reworked, replacing several methods and literals with new functions and constants to better handle data readouts from a device. The AbstractOneWireDevice class was also simplified and updated to complement these changes in the Dht11 class.

--- src/main/java/cn/tpkf/pi/devices/gpio/w1/AbstractOneWireDevice.java
+++ src/main/java/cn/tpkf/pi/devices/gpio/w1/AbstractOneWireDevice.java
@@ -8,30 +8,17 @@ import com.pi4j.plugin.pigpio.provider.gpio.digital.PiGpioDigitalInputProvider;
 import com.pi4j.plugin.pigpio.provider.gpio.digital.PiGpioDigitalOutputProvider;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-
-import java.util.concurrent.TimeUnit;
+import lombok.extern.slf4j.Slf4j;
 
 /**
+ * One Wire Device
  *
  * @author Harlan
+ * @email isharlan.hu@gmali.com
+ * @date 2023/12/14
  */
+@Slf4j
 public abstract class AbstractOneWireDevice extends AbstractGpioDevice {
-
-    /**
-     * The digitalOutput variable represents a digital output pin on a One Wire device.
-     * It is used to set the state of the pin, indicating whether it should be high or low.
-     * The value of the pin is set by calling the setValue() method defined in the DigitalOutput class.
-     * This pin is associated with the DigitalOutput instance of the device.
-     */
-    protected final DigitalOutputConfig digitalOutputConfig;
-
-    /**
-     * The digital input variable represents a digital input pin on a One Wire device.
-     * It is used to read the state of the pin, indicating whether it is high or low.
-     * The value of the pin is obtained by calling the getValue() method defined in the IBCMEnums interface.
-     * This pin is associated with the DigitalOutput instance of the device.
-     */
-    protected final DigitalInputConfig digitalInputConfig;
 
     @Getter
     protected WireState state;
@@ -66,7 +53,7 @@ public abstract class AbstractOneWireDevice extends AbstractGpioDevice {
      * @param shutdown      The shutdown state of the digital output pin.
      */
     protected AbstractOneWireDevice(DeviceManager deviceManager, String id, String name, IBCMEnums address,
-                                    DigitalState initial, DigitalState shutdown, WireState initState) {
+                                    DigitalState initial, DigitalState shutdown, PullResistance pull, WireState initState) {
         super(deviceManager, id, name, address);
         state = initState;
         if (DigitalState.LOW.equals(shutdown)) {
@@ -76,128 +63,39 @@ public abstract class AbstractOneWireDevice extends AbstractGpioDevice {
             onState = DigitalState.LOW;
             offState = DigitalState.HIGH;
         }
-        digitalOutputConfig = deviceManager.execute(c -> DigitalOutputConfigBuilder.newInstance(c)
-                .id(id + "-OUT")
-                .name(name)
-                .address(address.getValue())
-                .initial(initial)
-                .onState(onState)
-                .shutdown(shutdown)
-                .provider(PiGpioDigitalOutputProvider.class)
-                .build());
-        digitalInputConfig = deviceManager.execute(c -> DigitalInputConfigBuilder.newInstance(c)
-                .id(id + "-IN")
-                .name(name)
-                .address(address.getValue())
-                .provider(PiGpioDigitalInputProvider.class)
-                .build());
+        digitalOutput = deviceManager.execute(c -> {
+            DigitalOutputConfig config = DigitalOutputConfigBuilder.newInstance(c)
+                    .id(id + "-OUT")
+                    .name(name)
+                    .address(address.getValue())
+                    .initial(initial)
+                    .onState(onState)
+                    .shutdown(shutdown)
+                    .provider(PiGpioDigitalOutputProvider.class)
+                    .build();
+            return c.create(config);
+        });
+        digitalInput = deviceManager.execute(c -> {
+            DigitalInputConfig config = DigitalInputConfigBuilder.newInstance(c)
+                    .id(id + "-IN")
+                    .name(name)
+                    .address(address.getValue())
+                    .pull(pull)
+                    .provider(PiGpioDigitalInputProvider.class)
+                    .build();
+            return c.create(config);
+        });
         if (WireState.OUT == state) {
-            digitalOutput = deviceManager.execute(c -> c.create(digitalOutputConfig));
             digitalOutput.off();
-        } else {
-            digitalInput = deviceManager.execute(c -> c.create(digitalInputConfig));
         }
     }
 
-    protected DigitalOutput getOutput() {
-        try {
-            lock.lock();
-            if (WireState.OUT == state) {
-                return digitalOutput;
-            }
-            digitalOutput = deviceManager.execute(c -> {
-                digitalInput.shutdown(c);
-                c.shutdown(digitalOutput.getId());
-                return c.create(digitalOutputConfig);
-            });
-            state = WireState.OUT;
-            return digitalOutput;
-        } finally {
-            lock.unlock();
-        }
+    protected boolean isHigh() {
+        return digitalInput.isHigh();
     }
 
-    protected DigitalInput getInput() {
-        try {
-            lock.lock();
-            if (WireState.IN == state) {
-                return digitalInput;
-            }
-            digitalInput = deviceManager.execute(c -> {
-                digitalOutput.shutdown(c);
-                c.shutdown(digitalOutput.getId());
-                return c.create(digitalInputConfig);
-            });
-            state = WireState.IN;
-            return digitalInput;
-        } finally {
-            lock.unlock();
-        }
-    }
-
-    /**
-     * Turns on the digital output.
-     */
-    protected void outOn() {
-        try {
-            lock.lock();
-            DigitalOutput output = getOutput();
-            output.on();
-        } finally {
-            lock.unlock();
-        }
-    }
-
-    /**
-     * Turns off the digital output.
-     */
-    protected void outOff() {
-        try {
-            lock.lock();
-            DigitalOutput output = getOutput();
-            output.off();
-        } finally {
-            lock.unlock();
-        }
-    }
-
-    /**
-     * Executes a pulse on the digital output pin for a given duration in the specified time unit.
-     *
-     * @param duration    The duration of the pulse.
-     * @param timeUnit    The time unit of the duration.
-     * @param digitalState The digital state of the pulse.
-     */
-    protected void outPulse(int duration, TimeUnit timeUnit, DigitalState digitalState) {
-        try {
-            lock.lock();
-            DigitalOutput output = getOutput();
-            output.pulse(duration, timeUnit, digitalState);
-        } finally {
-            lock.unlock();
-        }
-    }
-
-    /**
-     * Executes a pulse on the digital output pin for a given duration in the specified time unit.
-     *
-     * @param duration    The duration of the pulse.
-     * @param timeUnit    The time unit of the duration.
-     */
-    protected void outPulse(int duration, TimeUnit timeUnit) {
-        outPulse(duration, timeUnit, onState);
-    }
-
-    protected DigitalState getInState() {
-        return getInput().state();
-    }
-
-    protected boolean isInHigh() {
-        return DigitalState.HIGH.equals(getInState());
-    }
-
-    protected boolean isInLow() {
-        return DigitalState.LOW.equals(getInState());
+    protected boolean isLow() {
+        return digitalInput.isLow();
     }
 
 
