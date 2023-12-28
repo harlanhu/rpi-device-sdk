4d854183caa91719c21a5d7f2a3af3e7fefce35d
harlan <isharlan.hu@gmail.com>
2023-12-28 16:03:21 +0800

    update test

--- src/test/java/cn/tpkf/rpi/devices/gpio/w1/Dht11Test.java
+++ src/test/java/cn/tpkf/rpi/devices/gpio/w1/Dht11Test.java
@@ -24,7 +24,7 @@ class Dht11Test {
     void detection() {
         DeviceManager deviceManager = new DeviceManager(context);
         Dht11 dht11 = new Dht11(deviceManager, "dht11", "温湿度传感器", BCMEnums.BCM_4);
-        assertNotNull(dht11.detection());
+        assertNotNull(dht11.detect());
         assertNotEquals(0.0, dht11.getHumidity());
         assertNotEquals(0.0, dht11.getTemperature());
     }
